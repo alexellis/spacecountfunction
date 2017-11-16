de162f46b5d7b1e1a4b5e0c1040294781e653d2f
Alex Ellis <alexellis2@gmail.com>
2017-11-16 22:03:28 +0000

    Update handler.js

--- webreq/handler.js
+++ webreq/handler.js
@@ -5,5 +5,6 @@ module.exports = (context, callback) => {
     
     request.get({uri: "http://api.open-notify.org/astros.json", json: true}, (err, data, jsonBody)=> {
                 callback(undefined, {jsonBody});
+        
     });
 }
