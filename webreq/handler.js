4d0350dfd7d78a787085daa18550a9617e0bf464
Alex Ellis <alexellis2@gmail.com>
2017-11-16 08:45:50 +0000

    Function

---
+++ webreq/handler.js
@@ -0,0 +1,9 @@
+"use strict"
+const request = require('request');
+
+module.exports = (context, callback) => {
+    request.get({uri: "http://api.open-notify.org/astros.json", json: true}, 
+            (err, data, jsonBody)=> {
+                callback(undefined, {jsonBody});
+            });
+}
